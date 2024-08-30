d7e7cff596ed524dc7b727aeed2e1d434918a54f
jovancvl <33637415+jovancvl@users.noreply.github.com>
2024-08-30 17:02:10 +0200

    updated website

--- src/main/java/jovancvl/javabotwebsite/Bot/Constants.java
+++ src/main/java/jovancvl/javabotwebsite/Bot/Constants.java
@@ -2,5 +2,5 @@ package jovancvl.javabotwebsite.Bot;
 
 public class Constants {
     public static long yourDaddy = 117282567626948613L;
-    public static String websiteURL = "http://localhost:12345/controls/";
+    public static String websiteURL = "http://javabotwebsite.onrender.com/controls/";
 }
