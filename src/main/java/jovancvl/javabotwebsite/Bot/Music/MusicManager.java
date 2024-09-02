0d5d3210f946ea67f56c981cc70cc89726aac75c
jovancvl <33637415+jovancvl@users.noreply.github.com>
2024-09-02 21:52:16 +0200

    trying ipv6 localhost

--- src/main/java/jovancvl/javabotwebsite/Bot/Music/MusicManager.java
+++ src/main/java/jovancvl/javabotwebsite/Bot/Music/MusicManager.java
@@ -50,7 +50,7 @@ public class MusicManager {
         client.addNode(
                 new NodeOptions.Builder()
                         .setName("localhost")
-                        .setServerUri("http://localhost:2333")
+                        .setServerUri("http://[::1]:2333")
                         .setPassword("youshallnotpass")
                         .build()
         );
