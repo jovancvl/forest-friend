30773b26be840e292642fbf459bb79b220380b51
jovancvl <33637415+jovancvl@users.noreply.github.com>
2024-09-05 15:48:45 +0200

    Update MusicManager.java

--- src/main/java/jovancvl/javabotwebsite/Bot/Music/MusicManager.java
+++ src/main/java/jovancvl/javabotwebsite/Bot/Music/MusicManager.java
@@ -52,7 +52,7 @@ public class MusicManager {
                         .setName("localhost")
                         .setServerUri("http://[::1]:2333")
                         .setPassword("youshallnotpass")
-                        .setHttpTimeout(200000) // in ms
+                        .setHttpTimeout(60000L) // in ms
                         .build()
         );
 
