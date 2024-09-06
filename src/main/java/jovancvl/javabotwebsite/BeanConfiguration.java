eaaf0f0c7c2bb8b185cae90c27eb2401f9ffa56f
jovancvl <33637415+jovancvl@users.noreply.github.com>
2024-09-06 19:35:32 +0200

    Update BeanConfiguration.java

--- src/main/java/jovancvl/javabotwebsite/BeanConfiguration.java
+++ src/main/java/jovancvl/javabotwebsite/BeanConfiguration.java
@@ -3,8 +3,11 @@ package jovancvl.javabotwebsite;
 import dev.arbjerg.lavalink.client.Helpers;
 import dev.arbjerg.lavalink.client.LavalinkClient;
 import jovancvl.javabotwebsite.Bot.Music.MusicManager;
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class BeanConfiguration {
@@ -15,5 +18,11 @@ public class BeanConfiguration {
         MusicManager.registerLavalinkNodes(lavalinkClient);
         MusicManager.registerLavalinkListeners(lavalinkClient);
         return lavalinkClient;
+    }
+
+    @Bean
+    public OkHttpClient okHttpClient(){
+        // Needed to set a read timeout longer than 10 seconds
+        return new OkHttpClient.Builder().readTimeout(30L, TimeUnit.SECONDS).build();
     }
 }
